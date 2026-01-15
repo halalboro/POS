f4c71b713693b99f0d1b55fc4b76d56ebc5b4c3b
halalboro <f20200947@pilani.bits-pilani.ac.in>
2026-01-15 11:41:57 +0000

    Cherry-pick: memory gateway working (7457014e) from TUM-DSE/microshell

--- scripts/ip_inst/common_infrastructure.tcl
+++ scripts/ip_inst/common_infrastructure.tcl
@@ -202,6 +202,66 @@ set_property -dict [list \
     CONFIG.ALL_PROBE_SAME_MU_CNT {2} \
 ] [get_ips ila_mem_gate_signal]
 
+create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name ila_mem_region_top_req_t
+set_property -dict [list \
+    CONFIG.C_NUM_OF_PROBES {32} \
+    CONFIG.C_PROBE0_WIDTH {5} \
+    CONFIG.C_PROBE1_WIDTH {2} \
+    CONFIG.C_PROBE2_WIDTH {1} \
+    CONFIG.C_PROBE3_WIDTH {1} \
+    CONFIG.C_PROBE4_WIDTH {1} \
+    CONFIG.C_PROBE5_WIDTH {4} \
+    CONFIG.C_PROBE6_WIDTH {6} \
+    CONFIG.C_PROBE7_WIDTH {4} \
+    CONFIG.C_PROBE8_WIDTH {1} \
+    CONFIG.C_PROBE9_WIDTH {48} \
+    CONFIG.C_PROBE10_WIDTH {28} \
+    CONFIG.C_PROBE11_WIDTH {1} \
+    CONFIG.C_PROBE12_WIDTH {1} \
+    CONFIG.C_PROBE13_WIDTH {6} \
+    CONFIG.C_PROBE14_WIDTH {1} \
+    CONFIG.C_PROBE15_WIDTH {1} \
+    CONFIG.C_PROBE16_WIDTH {5} \
+    CONFIG.C_PROBE17_WIDTH {2} \
+    CONFIG.C_PROBE18_WIDTH {1} \
+    CONFIG.C_PROBE19_WIDTH {1} \
+    CONFIG.C_PROBE20_WIDTH {1} \
+    CONFIG.C_PROBE21_WIDTH {4} \
+    CONFIG.C_PROBE22_WIDTH {6} \
+    CONFIG.C_PROBE23_WIDTH {4} \
+    CONFIG.C_PROBE24_WIDTH {1} \
+    CONFIG.C_PROBE25_WIDTH {48} \
+    CONFIG.C_PROBE26_WIDTH {28} \
+    CONFIG.C_PROBE27_WIDTH {1} \
+    CONFIG.C_PROBE28_WIDTH {1} \
+    CONFIG.C_PROBE29_WIDTH {6} \
+    CONFIG.C_PROBE30_WIDTH {1} \
+    CONFIG.C_PROBE31_WIDTH {1} \
+    CONFIG.C_EN_STRG_QUAL {1} \
+    CONFIG.ALL_PROBE_SAME_MU_CNT {2} \
+] [get_ips ila_mem_region_top_req_t]
+
+create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name ila_mem_gateway
+set_property -dict [list \
+    CONFIG.C_NUM_OF_PROBES {14} \
+    CONFIG.C_PROBE0_WIDTH {1} \
+    CONFIG.C_PROBE1_WIDTH {2} \
+    CONFIG.C_PROBE2_WIDTH {48} \
+    CONFIG.C_PROBE3_WIDTH {48} \
+    CONFIG.C_PROBE4_WIDTH {48} \
+    CONFIG.C_PROBE5_WIDTH {28} \
+    CONFIG.C_PROBE6_WIDTH {48} \
+    CONFIG.C_PROBE7_WIDTH {28} \
+    CONFIG.C_PROBE8_WIDTH {1} \
+    CONFIG.C_PROBE9_WIDTH {1} \
+    CONFIG.C_PROBE10_WIDTH {1} \
+    CONFIG.C_PROBE11_WIDTH {1} \
+    CONFIG.C_PROBE12_WIDTH {1} \
+    CONFIG.C_PROBE13_WIDTH {1} \
+    CONFIG.C_EN_STRG_QUAL {1} \
+    CONFIG.ALL_PROBE_SAME_MU_CNT {2} \
+] [get_ips ila_mem_gateway]
+
 # # Interconnect
 # create_ip -name axi_interconnect -vendor xilinx.com -library ip -version 2.1 -module_name axi_stream_interconnect_0
 # # Configure AXI Interconnect
